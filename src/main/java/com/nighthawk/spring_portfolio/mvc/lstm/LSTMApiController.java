baf5c05dfab31daa15e4021c6575fae694a6f2e5
YLu-1258 <maodou1258@gmail.com>
2024-02-12 22:26:25 -0800

    image render and endpoint complete

--- src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/LSTMApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/LSTMApiController.java
@@ -1,34 +1,21 @@
 package com.nighthawk.spring_portfolio.mvc.lstm;
 
-import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.nighthawk.spring_portfolio.mvc.lstm.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Base64;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nighthawk.spring_portfolio.mvc.lstm.*;
 
 @RestController
 @RequestMapping("/api/lstm")
@@ -44,6 +31,22 @@ public class LSTMApiController {
     /*
     GET Predictions for a stock
      */
+
+    public static String convertPNGToBase64(String filePath) {
+        String base64Data = null;
+        File file = new File(filePath);
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] bytes = new byte[(int) file.length()];
+            fileInputStream.read(bytes);
+
+            // Encode byte array to Base64
+            base64Data = Base64.getEncoder().encodeToString(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return base64Data;
+    }
+
     @GetMapping("/{ticker}")
     public ResponseEntity<String> getPredictions(@PathVariable String ticker) {
         String imagePath = "src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/resources/graphs/line_chart.png";
@@ -52,7 +55,8 @@ public class LSTMApiController {
         ArrayList<String> tickers = new ArrayList<String>(Arrays.asList("GOOGL", "AMZN", "AAPL", "TSLA", "WMT", "MSFT", "META", "COST", "LMT", "NOC", "UNH"));
         if (tickers.contains(ticker)) {
             LSTMMain model = new LSTMMain();
-            return new ResponseEntity<>( "HI", HttpStatus.OK);
+            String base64Data = convertPNGToBase64(imagePath);
+            return new ResponseEntity<>( base64Data, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST); 
         
