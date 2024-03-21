f1d9edc0c2e6041c8929c0f7320d4a1e07f72268
DavidVasilev1 <davidkvasilev@gmail.com>
2024-03-21 08:38:54 -0700

    updating LSTM implementation new

--- src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/LSTMMain.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/LSTMMain.java
@@ -1,32 +1,32 @@
-package com.nighthawk.spring_portfolio.mvc.lstm;
+// package com.nighthawk.spring_portfolio.mvc.lstm;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.datavec.api.records.reader.RecordReader;
-import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
-import org.datavec.api.split.FileSplit;
-import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
-import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
-import org.nd4j.linalg.dataset.DataSet;
-import org.nd4j.linalg.dataset.SplitTestAndTrain;
+// import org.datavec.api.records.reader.RecordReader;
+// import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+// import org.datavec.api.split.FileSplit;
+// import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+// import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+// import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+// import org.nd4j.linalg.dataset.DataSet;
+// import org.nd4j.linalg.dataset.SplitTestAndTrain;
 
-import java.io.File;
+// import java.io.File;
 
-import com.nighthawk.spring_portfolio.mvc.lstm.LSTMNetModel;
-import com.nighthawk.spring_portfolio.mvc.lstm.LSTMTrainerTester;
+// import com.nighthawk.spring_portfolio.mvc.lstm.LSTMNetModel;
+// import com.nighthawk.spring_portfolio.mvc.lstm.LSTMTrainerTester;
 
-public class LSTMMain {
-    public LSTMMain(String ticker1) {
-        String directory = "src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/resources/stock_data";
-        String ticker = ticker1; // stock name
+// public class LSTMMain {
+//     public LSTMMain(String ticker1) {
+//         String directory = "src/main/java/com/nighthawk/spring_portfolio/mvc/lstm/resources/stock_data";
+//         String ticker = ticker1; // stock name
 
-        System.out.println("Create dataSet iterator...");
+//         System.out.println("Create dataSet iterator...");
    
-        LSTMTrainerTester iterator1 = new LSTMTrainerTester(directory, ticker, 4, 1, 32, 10);
+//         LSTMTrainerTester iterator1 = new LSTMTrainerTester(directory, ticker, 4, 1, 32, 10);
             
-        MultiLayerNetwork net = LSTMNetModel.buildLstmNetworks(iterator1.getFeatures(), iterator1.getLabels());
+//         MultiLayerNetwork net = LSTMNetModel.buildLstmNetworks(iterator1.getFeatures(), iterator1.getLabels());
 
-        iterator1.TrainAndTestModel(net);
-    }
-}
+//         iterator1.TrainAndTestModel(net);
+//     }
+// }
