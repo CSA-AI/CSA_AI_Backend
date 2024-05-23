6985680df0f54ed11222eb6ba44a6dea05791916
Tay Kim <taykimpro@gmail.com>
2024-05-23 02:45:42 -0700

    manually adding asl

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/frames/FrameApiController.java
@@ -0,0 +1,5 @@
+package com.nighthawk.spring_portfolio.mvc.frames;
+
+public class FrameApiController {
+    
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/frames/FrameJpaRepository.java
@@ -0,0 +1,7 @@
+package com.nighthawk.spring_portfolio.mvc.frames;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FrameJpaRepository extends JpaRepository<Frame, Long> {
+}
