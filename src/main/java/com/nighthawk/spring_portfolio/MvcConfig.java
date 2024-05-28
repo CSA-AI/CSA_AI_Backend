5715dce44f3e4788fb9526d3dffaa210610062ec
Emaad-Mir <emaadidrismir@gmail.com>
2024-05-28 00:44:36 -0700

    hey

--- src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/MvcConfig.java
@@ -1,7 +1,10 @@
 package com.nighthawk.spring_portfolio;
 
-import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -18,11 +21,11 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(
-            "https://csa-ai-frontend.vercel.app"
-        ).allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .allowedHeaders("authorization", "content-type", "x-csrf-token")
-        .exposedHeaders("authorization")
-        .allowCredentials(true);
+        registry.addMapping("/**")
+            .allowedOrigins("https://csa-ai-frontend.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("authorization", "content-type", "x-csrf-token")
+            .exposedHeaders("authorization")
+            .allowCredentials(true);
     }
 }
